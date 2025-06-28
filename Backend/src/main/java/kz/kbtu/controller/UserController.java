4d72c3e9118258da01737af6639e26477dcfcaad
gaukhar-tazhiyeva <tazhiyevagauhar@gmail.com>
2025-06-28 21:50:35 +0600

    new commit1

--- Backend/src/main/java/kz/kbtu/controller/UserController.java
+++ Backend/src/main/java/kz/kbtu/controller/UserController.java
@@ -1,4 +1,21 @@
 package kz.kbtu.controller;
 
+import kz.kbtu.entity.User;
+import kz.kbtu.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+
 public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
